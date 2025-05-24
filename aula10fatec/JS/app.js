0b17ba847e958762d5954745094e30a28d16ad3a
Gabriel Alves <137641904+GabrielvFrei@users.noreply.github.com>
2025-05-24 11:32:03 -0300

    feat: calculadora Fatec em JS

---
+++ aula10fatec/JS/app.js
@@ -0,0 +1,50 @@
+function teste(){
+    alert('Ola mundo')
+}
+
+// Variaveis
+
+let idade = 25
+console.log(idade)
+
+let novaIdade = idade + 1
+console.log(novaIdade)
+
+idade = 30
+console.log(idade)
+
+//tipagem variaveis
+
+let pdiddy = 11112463;
+
+let nome = "enzo"
+let sobrenome = "oliveira brito"
+let familiaNome = "maluco"
+
+console.log(nome)
+console.log(sobrenome)
+console.log(familiaNome)
+
+//variavel boolean
+
+let verdadeiro = true
+let falso = false
+
+console.log(verdadeiro)
+console.log(falso)
+
+//variavel indefinida (undefined)
+
+let indefinida
+console.log(indefinida)
+
+//teste
+
+function entrada(){
+    let nome = prompt('Digite seu nome')
+    alert(nome)
+}
+
+function confirmar(){
+    let resultado = confirm('Deseja confirmar?')
+}
---
+++ aula10fatec/JS/calculadora.js
@@ -0,0 +1,19 @@
+function calcular(operacao){
+    let valor1 = parseFloat(document.getElementById('n1').value)
+    let valor2 = parseFloat(document.getElementById('n2').value)
+
+    let resultado
+    if(operacao == 'somar'){
+        resultado = valor1 + valor2
+    }
+    if(operacao == 'subtrair'){
+        resultado = valor1 - valor2
+    }
+    if(operacao == 'multiplicar'){
+        resultado = valor1 * valor2
+    }
+    if(operacao == 'dividir'){
+        resultado = valor1 / valor2
+    }
+    document.getElementById('resultado').innerText='Resultado = '+resultado
+}
